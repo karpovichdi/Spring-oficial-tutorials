c92c61112fefc57014a0bcf8354847e12f5a7c1b
Karpovich Dmitry <karpovichdi@hotmail.com>
2020-04-13 16:56:43 +0300

    Added: Test controllers.

---
+++ src/main/java/com/karpovichdi/training/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+package com.karpovichdi.training.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.karpovichdi.training.models.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println("");
+    }
+}
